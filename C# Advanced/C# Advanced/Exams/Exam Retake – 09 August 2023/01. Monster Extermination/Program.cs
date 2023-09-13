2dbfb5304e4ad98f84792d1c747b7aa1a3780471
BorisAnastasov <boris_ywu@abv.bg>
2023-09-13 21:16:17 +0300

    almost solved

---
+++ C# Advanced/C# Advanced/Exams/Exam Retake – 09 August 2023/01. Monster Extermination/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _01._Monster_Extermination
+{
+       internal class Program
+       {
+              static void Main(string[] args)
+              {
+                     int monstersKilled = 0;
+                     Queue<int> armors = new Queue<int>();
+                     List<int> arr1 = Console.ReadLine().Split(",").Select(int.Parse).ToList();
+                     arr1.ForEach(i => armors.Enqueue(i));
+
+                     Stack<int> strikes = new Stack<int>();
+                     List<int> arr2 = Console.ReadLine().Split(",").Select(int.Parse).ToList();
+                     arr2.ForEach(i => strikes.Push(i));
+
+                     while (strikes.Any() && armors.Any())
+                     {
+                            int armor = armors.Dequeue();
+                            int strike = strikes.Pop();
+                            if (armor < strike)
+                            {
+                                   strike -= armor;
+                                   strikes.Push(strikes.Pop() + strike);
+                                   monstersKilled++;
+                            }
+                            else if (armor > strike)
+                            {
+                                   armor -= strike;
+                                   armors.Enqueue(armor);
+                            }
+                            else
+                            {
+                                   monstersKilled++;
+                            }
+                     }
+                     if (!strikes.Any())
+                     {
+                            Console.WriteLine("The soldier has been defeated.");
+                     }
+                     else if (!armors.Any())
+                     {
+                            Console.WriteLine("All monsters have been killed!");
+
+                     }
+                     Console.WriteLine($"Total monsters killed: {monstersKilled}");
+              }
+       }
+}
