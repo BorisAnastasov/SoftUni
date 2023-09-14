80af4414e39b77071621b7177b3e7f57ac720dd2
BorisAnastasov <boris_ywu@abv.bg>
2023-09-14 14:59:33 +0300

    solving

---
+++ C# Advanced/C# Advanced/Exams/Exam Retake – 09 August 2023/02. Delivery Boy/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace _02._Delivery_Boy
+{
+       internal class Program
+       {
+              public static int rowBoy = 0;
+              public static int colBoy = 0;
+              static void Main(string[] args)
+              {
+                     int[] paramaters = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                     int rows, cols;
+                     rows = paramaters[0];
+                     cols = paramaters[1];
+                     int[,] matrix = new int[rows, cols];
+
+                     for (int i = 0; i < matrix.GetLength(0); i++)
+                     {
+                            char[] line = Console.ReadLine().ToCharArray();
+                            for (int z = 0; z < matrix.GetLength(1); z++)
+                            {
+                                   if (line[z] == 'B')
+                                   {
+                                          rowBoy = i;
+                                          colBoy = z;
+                                   }
+                                   matrix[i, z] = line[z];
+                            }
+                     }
+                     string cmd;
+                     while ((cmd = Console.ReadLine()) != null)
+                     {
+                            matrix[rowBoy, colBoy] = '.';
+
+                            if (cmd == "up")
+                            {
+                                   colBoy++;
+
+                            }
+                            else if (cmd == "down")
+                            {
+                                   colBoy--;
+
+                            }
+                            else if (cmd == "right")
+                            {
+                                   rowBoy++;
+                            }
+                            else //left
+                            {
+                                   rowBoy--;
+                            }
+                     }
+              }
+
+              public bool IsLate(int row, int col, ref int[,] matrix)
+              {
+                     return matrix.GetLength(0) >= row + 1 &&
+                     matrix.GetLength(1) >= col + 1 &&
+                     0 <= row &&
+                     0 <= col;
+              }
+
+              public int[] DeliveryMover(int row, int col, ref int[,] matrix)
+              {
+
+                     if ()
+              }
+       }
+}
