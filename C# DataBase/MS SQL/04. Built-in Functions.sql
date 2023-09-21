915740fc18f0201013cb8546a9a2b9ec758c1bc0
Boris Anastasov <114017122+BorisAnastasov@users.noreply.github.com>
2023-09-21 14:34:50 +0300

    Add files via upload

---
+++ C# DataBase/MS SQL/04. Built-in Functions.sql
@@ -0,0 +1,147 @@
+--Part I – Queries for SoftUni Database
+USE SoftUni;
+
+--1. Find Names of All Employees by First Name
+SELECT FirstName,LastName
+FROM Employees
+WHERE LEFT(FirstName,2) = 'Sa';
+
+--2. Find Names of All Employees by Last Name 
+SELECT FirstName,LastName
+FROM Employees
+WHERE LastName LIKE '%ei%';
+
+--3. Find First Names of All Employees
+SELECT FirstName
+FROM Employees
+WHERE DepartmentID IN (3,10) AND YEAR(HireDate) BETWEEN 1995 AND 2005;
+
+--04. Find All Employees Except Engineers
+SELECT FirstName,LastName
+FROM Employees
+WHERE JobTitle NOT LIKE '%engineer%';
+
+--05. Find Towns with Name Length
+SELECT [Name]
+FROM Towns
+WHERE LEN([Name]) IN (5,6)
+ORDER BY [Name] ASC;
+
+--06. Find Towns Starting With
+SELECT TownID,[Name]
+FROM Towns
+WHERE LEFT([Name],1) IN ('M','K','B','E')
+ORDER BY [Name] ASC;
+
+--07. Find Towns Not Starting With
+SELECT TownID,[Name]
+FROM Towns	
+WHERE NOT LEFT([Name],1) IN ('R','B','D')
+ORDER BY [Name] ASC;
+
+--08. Create View Employees Hired After 2000 Year
+CREATE VIEW V_EmployeesHiredAfter2000 AS
+SELECT FirstName,LastName
+FROM Employees
+WHERE YEAR(HireDate)>2000;
+
+--09. Length of Last Name
+SELECT FirstName, LastName
+FROM Employees
+WHERE LEN(LastName) = 5;
+
+--10. Rank Employees by Salary
+SELECT EmployeeID,FirstName,LastName,Salary,DENSE_RANK() OVER (PARTITION BY Salary ORDER BY EmployeeID) AS [Rank]
+FROM Employees
+WHERE Salary BETWEEN 10000 AND 50000
+ORDER BY Salary DESC;
+
+--11. Find All Employees with Rank 2
+WITH RankScores AS (
+SELECT EmployeeID,FirstName,LastName,Salary,DENSE_RANK() OVER (PARTITION BY Salary ORDER BY EmployeeID) AS [Rank]
+FROM Employees
+)
+SELECT *
+FROM RankScores
+WHERE Salary BETWEEN 10000 AND 50000 AND [Rank]= 2  
+ORDER BY Salary DESC;
+
+--Part II – Queries for Geography Database
+USE [Geography];
+
+--12. Countries Holding 'A' 3 or More Times
+SELECT CountryName, IsoCode
+FROM Countries
+WHERE (LEN(CountryName) - LEN(REPLACE(UPPER(CountryName),'A',''))) >= 3
+ORDER BY IsoCode;
+
+--13. Mix of Peak and River Names
+SELECT PeakName,RiverName ,LOWER(PeakName + RIGHT(RiverName,LEN(RiverName)-1)) AS Mix
+FROM Peaks,Rivers
+WHERE LOWER(RIGHT(PeakName,1)) = LOWER(LEFT(RiverName,1))
+ORDER BY Mix;
+
+--Part III – Queries for Diablo Database
+USE Diablo;
+
+--14. Games from 2011 and 2012 Year
+SELECT TOP(50) [Name],FORMAT([Start],'yyyy-MM-dd') AS Start
+FROM Games
+WHERE YEAR([Start]) IN (2011,2012)
+ORDER BY 
+[Start] ASC,
+[Name] ASC;
+
+--15. User Email Providers
+SELECT Username,
+SUBSTRING(Email, CHARINDEX('@', Email) + 1, LEN(Email)) AS EmailProvider
+FROM Users
+ORDER BY 
+EmailProvider,
+Username;
+
+--16. Get Users with IP Address Like Pattern
+SELECT Username,IpAddress
+FROM Users
+WHERE IpAddress LIKE '___.1%.%.___'
+ORDER BY Username;
+
+--17. Show All Games with Duration & Part of the Day
+SELECT [Name],
+CASE 
+     WHEN DATEPART(HOUR,[Start]) >= 0 AND DATEPART(HOUR,[Start]) < 12 THEN 'Morning'
+     WHEN DATEPART(HOUR,[Start]) >= 12 AND DATEPART(HOUR,[Start]) < 18 THEN 'Afternoon'
+     WHEN DATEPART(HOUR,[Start]) >= 18 AND DATEPART(HOUR,[Start]) < 24 THEN 'Evening'
+END AS 'Part of the Day',
+CASE 
+     WHEN Duration <= 3 THEN 'Extra Short'
+     WHEN Duration BETWEEN 4 AND 6 THEN 'Short'
+     WHEN Duration > 6 THEN 'Long'
+	 ELSE 'Extra Long'
+END AS 'Duration'
+FROM Games
+ORDER BY [Name] ASC,Duration ASC,[Part of the Day] ASC
+
+--Part IV – Date Functions Queries
+USE Orders;
+
+--18. Orders Table
+SELECT ProductName, OrderDate, 
+DATEADD(DAY, 3, OrderDate) AS [Pay Due],
+DATEADD(MONTH, 1, OrderDate) AS [Deliver Due]
+FROM Orders
+
+--19. People Table
+CREATE TABLE People(
+Id INT IDENTITY(1,1) PRIMARY KEY,
+[Name] NVARCHAR(50),
+Birthdate DATETIME
+)
+
+SELECT [Name],
+DATEDIFF(YEAR, Birthdate, GETDATE()) AS [Age in Years],
+DATEDIFF(MONTH, Birthdate, GETDATE()) AS [Age in Months],
+DATEDIFF(DAY, Birthdate, GETDATE()) AS [Age in Days],
+DATEDIFF(MINUTE, Birthdate, GETDATE()) AS [Age in Minutes]
+FROM People
+
