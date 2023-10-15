03c82c3f24c32eb94341e09bfe243c57ef2f2894
Boris Anastasov <114017122+BorisAnastasov@users.noreply.github.com>
2023-10-15 20:13:45 +0300

    Add files via upload

---
+++ C# DataBase/MS SQL/CRUD/21. Increase Salaries.sql
@@ -0,0 +1,11 @@
+--modified
+UPDATE Employees
+SET Salary *= 1.12
+WHERE DepartmentID IN(1, 2, 4, 11);
+
+SELECT Salary FROM Employees;
+
+--original
+UPDATE Employees
+SET Salary = Salary / 1.12
+WHERE DepartmentID IN (1, 2, 4, 11);
