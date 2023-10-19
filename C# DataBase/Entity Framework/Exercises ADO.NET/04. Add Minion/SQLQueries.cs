40691e31ff963668dd3c06248415ced9fca63c10
BorisAnastasov <boris_ywu@abv.bg>
2023-10-19 23:06:19 +0300

    commit

---
+++ C# DataBase/Entity Framework/Exercises ADO.NET/04. Add Minion/SQLQueries.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04._Add_Minion
+{
+       public static class SQLQueries
+       {
+              public const string SelectVillains = @"SELECT Id FROM Villains WHERE Name = @Name";
+              public const string SelectMinions = @"SELECT Id FROM Minions WHERE Name = @Name";
+              public const string InsertMinionsVillains = @"INSERT INTO MinionsVillains (MinionId, VillainId) VALUES (@minionId, @villainId)";
+              public const string InsertVillains = @"INSERT INTO Villains (Name, EvilnessFactorId)  VALUES (@villainName, 4)";
+              public const string InsertMinions = @"INSERT INTO Minions (Name, Age, TownId) VALUES (@name, @age, @townId)";
+              public const string InsertTowns = @"INSERT INTO Towns (Name) VALUES (@townName)";
+              public const string SelectTowns = @"SELECT Id FROM Towns WHERE Name = @townName";
+       }
+}
---
+++ C# DataBase/Entity Framework/Exercises ADO.NET/04. Add Minion/Startup.cs
@@ -0,0 +1,39 @@
+﻿using _01._Initial_Setup;
+using Microsoft.Data.SqlClient;
+
+namespace _04._Add_Minion
+{
+       internal class Startup
+       {
+              static void Main(string[] args)
+              {
+                     string[] inputMinion = Console.ReadLine().Split(" ").Skip(1).ToArray();
+                     string nameVillain = Console.ReadLine().Split(" ").Skip(1).ToArray()[0];
+
+                     string nameMinion = inputMinion[0];
+                     int nameAge = int.Parse(inputMinion[1]);
+                     string townMinion = inputMinion[2];
+
+
+
+                     using SqlConnection connection = new SqlConnection(Config.ConnectionString);
+                     connection.Open();
+
+
+                     try
+                     {
+                            SqlCommand sqlCommandVillain = new SqlCommand(SQLQueries.SelectVillains, connection);
+                            sqlCommandVillain.Parameters.AddWithValue("@Name", nameVillain);
+                            SqlDataReader reader = sqlCommandVillain.ExecuteReader();
+
+
+                     }
+                     catch (Exception)
+                     {
+
+                            throw;
+                     }
+
+              }
+       }
+}
