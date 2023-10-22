31cd3f957d3a8cc4d744a0a1cf1da8fa778356d9
BorisAnastasov <boris_ywu@abv.bg>
2023-10-22 10:06:36 +0300

    commit

---
+++ C# DataBase/Entity Framework/Exercises ADO.NET/05. Change Town Names Casing/SQLQueries.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._Minion_Names
+{
+       public static class SQLQueries
+       {
+              public const string UpdateTowns =
+              @"UPDATE Towns
+                SET Name = UPPER(Name)
+               WHERE CountryCode = (SELECT c.Id FROM Countries AS c WHERE c.Name = @countryName)";
+
+              public const string SelectTowns = 
+              @" SELECT t.Name 
+                 FROM Towns as t
+                 JOIN Countries AS c ON c.Id = t.CountryCode
+                WHERE c.Name = @countryName";
+       }
+}
---
+++ C# DataBase/Entity Framework/Exercises ADO.NET/05. Change Town Names Casing/StartUp.cs
@@ -0,0 +1,61 @@
+﻿using _01._Initial_Setup;
+using _03._Minion_Names;
+using Microsoft.Data.SqlClient;
+using System.Text;
+
+namespace _05._Change_Town_Names_Casing
+{
+       internal class StartUp
+       {
+              static void Main(string[] args)
+              {
+                     string countryName = Console.ReadLine();
+
+                     StringBuilder sb = new StringBuilder();
+
+                     using SqlConnection conection = new SqlConnection(Config.ConnectionString);
+                     conection.Open();
+
+                     SqlTransaction transaction = conection.BeginTransaction();
+
+                     try
+                     {
+                            SqlCommand updateTownsCmd = new SqlCommand(SQLQueries.UpdateTowns, conection, transaction);
+                            updateTownsCmd.Parameters.AddWithValue("@countryName", countryName);
+                            int affectedRows = updateTownsCmd.ExecuteNonQuery();
+
+                            if (affectedRows == 0)
+                            {
+                                   Console.WriteLine("No town names were affected.");
+                                   return;
+                            }
+
+                            sb.AppendLine($"{affectedRows} town names were affected.");
+
+                            SqlCommand selectTownsCmd = new SqlCommand(SQLQueries.SelectTowns, conection, transaction);
+                            selectTownsCmd.Parameters.AddWithValue("@countryName", countryName);
+
+                            using (SqlDataReader reader = selectTownsCmd.ExecuteReader()) 
+                            {
+                                   List<string> towns = new List<string>();
+                                   while (reader.Read())
+                                   {
+                                          towns.Add(reader["Name"].ToString());
+                                   }
+                                   sb.AppendLine($"[{string.Join(", ", towns)}]");
+
+                            }
+
+                            Console.WriteLine(sb.ToString());
+
+                            transaction.Commit();
+                     }
+                     catch (Exception)
+                     {
+                            transaction.Rollback();
+                            Console.WriteLine("Error");
+                     }
+
+              }
+       }
+}
