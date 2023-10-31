e74c64b366bc20f5200cf09e7dbf1c79e3b8c578
BorisAnastasov <boris_ywu@abv.bg>
2023-10-31 11:29:45 +0200

    commit

---
+++ C# DataBase/Entity Framework/Entity Framework Introduction/11. Find Latest 10 Projects/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System.Globalization;
+using System.Text;
+
+namespace SoftUni
+{
+       public class StartUp
+       {
+              static void Main(string[] args)
+              {
+                     using SoftUniContext context = new SoftUniContext();
+
+                     string result = GetLatestProjects(context);
+
+                     Console.WriteLine(result);
+              }
+
+              public static string GetLatestProjects(SoftUniContext context)
+              {
+                     StringBuilder sb = new StringBuilder();
+
+                     var projects = context.Projects
+                                                 .OrderByDescending(p => p.StartDate)
+                                                 .Take(10)
+                                                 .OrderBy(p => p.Name)
+                                                 .Select(p => new
+                                                 {
+                                                        p.Name,
+                                                        p.Description,
+                                                        StartDate = p.StartDate.ToString("M/d/yyyy h:mm:ss tt",CultureInfo.InvariantCulture)
+                                                 })
+                                                 .ToArray();
+                     foreach (var p in projects)
+                     {
+                            sb.AppendLine(p.Name);
+                            sb.AppendLine(p.Description);
+                            sb.AppendLine(p.StartDate);
+                     }
+                     return sb.ToString().Trim();
+              }
+       }
+}
---
+++ C# DataBase/Entity Framework/Entity Framework Introduction/12. Increase Salaries/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System.Globalization;
+using System.Text;
+
+namespace SoftUni
+{
+       public class StartUp
+       {
+              static void Main(string[] args)
+              {
+                     using SoftUniContext context = new SoftUniContext();
+
+                     string result = IncreaseSalaries(context);
+
+                     Console.WriteLine(result);
+              }
+
+              public static string IncreaseSalaries(SoftUniContext context)
+              {
+                     StringBuilder sb = new StringBuilder();
+
+                     
+
+                     return sb.ToString().Trim();
+              }
+       }
+}
