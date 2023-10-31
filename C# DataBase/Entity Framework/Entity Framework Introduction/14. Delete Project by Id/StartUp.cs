c0bc4defa33b1f501b21c3e1d75980f5d284a940
BorisAnastasov <boris_ywu@abv.bg>
2023-10-31 13:08:02 +0200

    commit

---
+++ C# DataBase/Entity Framework/Entity Framework Introduction/14. Delete Project by Id/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using SoftUni.Data;
+using SoftUni.Models;
+using System.Globalization;
+using System.Text;
+
+namespace SoftUni
+{
+       public class StartUp
+       {
+              static void Main(string[] args)
+              {
+                     using SoftUniContext context = new SoftUniContext();
+
+                     string result = DeleteProjectById(context);
+
+                     Console.WriteLine(result);
+              }
+
+              public static string DeleteProjectById(SoftUniContext context)
+              {
+                     StringBuilder sb = new StringBuilder();
+
+                     var project = context.Projects.Find(2);
+
+                     var employeesProjects = context.EmployeesProjects
+                                                 .Where(ep => ep.Project == project);
+                     foreach (var ep in employeesProjects)
+                     {
+                            context.EmployeesProjects.Remove(ep);
+                     }
+                     context.SaveChanges();
+
+                     context.Projects.Remove(project);
+
+                     context.SaveChanges();
+
+                     var projects = context.Projects
+                                                 .Take(10);
+
+                     foreach (var p in projects)
+                     {
+                            sb.AppendLine(p.Name);
+                     }
+
+                     return sb.ToString().Trim();
+              }
+       }
+}
