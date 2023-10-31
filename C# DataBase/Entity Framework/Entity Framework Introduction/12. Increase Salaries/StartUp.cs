306e0cac378959a79cb322f7a770fd1d61f5428b
BorisAnastasov <boris_ywu@abv.bg>
2023-10-31 11:56:52 +0200

    commit

--- C# DataBase/Entity Framework/Entity Framework Introduction/12. Increase Salaries/StartUp.cs
+++ C# DataBase/Entity Framework/Entity Framework Introduction/12. Increase Salaries/StartUp.cs
@@ -1,4 +1,6 @@
-﻿using SoftUni.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using SoftUni.Data;
 using SoftUni.Models;
 using System.Globalization;
 using System.Text;
@@ -20,7 +22,33 @@ namespace SoftUni
               {
                      StringBuilder sb = new StringBuilder();
 
-                     
+                     var departmentTypesForIncrese = new[] { "Engineering", "Tool Design", "Marketing", "Information Services" };
+
+                     var employees = context.Employees
+                                                 .Where(e => departmentTypesForIncrese.Contains(e.Department.Name))
+                                                 .ToArray();
+
+                     foreach (var e in employees)
+                     {
+                            e.Salary *= (decimal)1.12;
+                     }
+
+                     context.SaveChanges();
+
+                     var increaseSalaryEmployees = employees
+                                                               .Select(e => new
+                                                               {
+                                                                      e.FirstName,
+                                                                      e.LastName,
+                                                                      Salary = e.Salary.ToString("f2")
+                                                               })
+                                                               .OrderBy(e => e.FirstName)
+                                                               .ThenBy(e => e.LastName)
+                                                               .ToArray();
+                     foreach (var e in increaseSalaryEmployees)
+                     {
+                            sb.AppendLine($"{e.FirstName} {e.LastName} (${e.Salary})");
+                     }
 
                      return sb.ToString().Trim();
               }
