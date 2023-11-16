cedb3724e421617d75647ae6e2f9313bb8d709b3
BorisAnastasov <boris_ywu@abv.bg>
2023-11-16 20:56:07 +0200

    task 1 solved

--- C# DataBase/Entity Framework/JavaScript Object Notation - JSON/01. ProductShop/ProductShop/Models/User.cs
+++ C# DataBase/Entity Framework/JavaScript Object Notation - JSON/01. ProductShop/ProductShop/Models/User.cs
@@ -1,28 +1,30 @@
 ﻿namespace ProductShop.Models
 {
-    using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations.Schema;
+       using Newtonsoft.Json;
+       using System.Collections.Generic;
+       using System.ComponentModel.DataAnnotations.Schema;
+       using System.Text.Json.Serialization;
 
-    public class User
-    {
-        public User()
-        {
-            ProductsSold = new List<Product>();
-            ProductsBought = new List<Product>();
-        }
+       public class User
+       {
+              public User()
+              {
+                     ProductsSold = new List<Product>();
+                     ProductsBought = new List<Product>();
+              }
 
-        public int Id { get; set; }
+              public int Id { get; set; }
 
-        public string? FirstName { get; set; }
+              public string? FirstName { get; set; }
 
-        public string LastName { get; set; } = null!;
+              public string LastName { get; set; } = null!;
 
-        public int? Age { get; set; }
+              public int? Age { get; set; }
 
-        [InverseProperty("Seller")]
-        public ICollection<Product> ProductsSold { get; set; }
+              [InverseProperty("Seller")]
+              public ICollection<Product> ProductsSold { get; set; }
 
-        [InverseProperty("Buyer")]
-        public ICollection<Product> ProductsBought { get; set; }
-    }
+              [InverseProperty("Buyer")]
+              public ICollection<Product> ProductsBought { get; set; }
+       }
 }
--- C# DataBase/Entity Framework/JavaScript Object Notation - JSON/01. ProductShop/ProductShop/StartUp.cs
+++ C# DataBase/Entity Framework/JavaScript Object Notation - JSON/01. ProductShop/ProductShop/StartUp.cs
@@ -1,9 +1,34 @@
-﻿namespace ProductShop
+﻿using ProductShop.Data;
+using ProductShop.Models;
+using Newtonsoft.Json;
+
+namespace ProductShop
 {
-    public class StartUp
-    {
-        public static void Main()
-        {
-        }
-    }
+       public class StartUp
+       {
+              public static void Main()
+              {
+                     ProductShopContext context = new ProductShopContext();
+
+                     context.Database.EnsureDeleted();
+                     context.Database.EnsureCreated();
+
+
+                     string json = File.ReadAllText("../../../Datasets/users.json");
+
+                     Console.WriteLine(ImportUsers(context, json));
+
+              }
+
+              //01. Import Users 
+              public static string ImportUsers(ProductShopContext context, string inputJson)
+              {
+                     var users = JsonConvert.DeserializeObject<User[]>(inputJson);
+
+                     context.Users.AddRange(users);
+                     context.SaveChanges();      
+
+                     return $"Successfully imported {users.Length}";
+              }
+       }
 }
