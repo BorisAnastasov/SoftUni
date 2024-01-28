5bd9f8e4fd202af55cdf6a1cd782838610922004
BorisAnastasov <boris_ywu@abv.bg>
2024-01-28 19:51:55 +0200

    commit

---
+++ C# Web/ASP.NET Fundamentals/My Projects/SugarDecoration/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SugarDecoration.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
