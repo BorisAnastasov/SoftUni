c6a715ab3ae215d93a86c9c5e461d150480da09d
BorisAnastasov <boris_ywu@abv.bg>
2024-02-04 18:17:29 +0200

    commit

---
+++ C# Web/ASP.NET Fundamentals/My Projects/ForumApp/Forum.Data/Configuration/PostEntityConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Forum.Data.Models;
+using Forum.Data.Seeding;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Forum.Data.Configuration
+{
+	public class PostEntityConfiguration : IEntityTypeConfiguration<Post>
+	{
+		private readonly PostSeeder _postseeder;
+
+        public PostEntityConfiguration()
+        {
+            this._postseeder = new PostSeeder();
+        }
+        public void Configure(EntityTypeBuilder<Post> builder)
+		{
+			builder.
+					HasData(this._postseeder.GeneratePosts().ToArray());
+
+		}
+	}
+}
---
+++ C# Web/ASP.NET Fundamentals/My Projects/ForumApp/Forum.Data/ForumDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Forum.Data.Models;
+using Forum.Data.Configuration;
+
+namespace Forum.Data
+{
+	public class ForumDbContext:DbContext
+	{
+        public ForumDbContext(DbContextOptions<ForumDbContext> options)
+        :base(options)
+        {
+            
+        }
+
+        public DbSet<Post> Posts { get; set; } = null!;
+
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.ApplyConfiguration(new PostEntityConfiguration());
+
+
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
