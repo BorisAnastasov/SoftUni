ccd6eca83f49b8cf48c23c9f9e217d41c3e49dfb
BorisAnastasov <boris_ywu@abv.bg>
2024-02-09 11:30:26 +0200

    commit

---
+++ C# Web/ASP.NET Fundamentals/My Projects/TaskBoardApp/TaskBoardApp/Data/Models/Board.cs
@@ -0,0 +1,15 @@
+﻿namespace TaskBoardApp.Data.Models
+{
+	using System.ComponentModel.DataAnnotations;
+	using static TaskBoardApp.Data.DataConstants.Board;
+	public class Board
+	{
+		public int Id { get; set; }
+
+		[Required]
+		[MaxLength(BoardMaxName)]
+		public string Name { get; set; } = null!;
+
+		public IEnumerable<Task> Tasks { get; set; } = new List<Task>();
+	}
+}
---
+++ C# Web/ASP.NET Fundamentals/My Projects/TaskBoardApp/TaskBoardApp/Data/TaskBoardAppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TaskBoardApp.Data.Models;
+using Task = TaskBoardApp.Data.Models.Task;
+
+namespace TaskBoardApp.Data
+{
+    public class TaskBoardAppDbContext : IdentityDbContext<IdentityUser>
+    {
+        private IdentityUser TestUser { get; set; }
+        private Board OpenBoard { get; set; }
+        private Board InProgressBoard { get; set; }
+        private Board DoneBoard { get; set; }
+
+        public TaskBoardAppDbContext(DbContextOptions<TaskBoardAppDbContext> options)
+            : base(options)
+        {
+            Database.Migrate();
+        }
+
+        public DbSet<Board> Board { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+            builder.Entity<Task>()
+            .HasOne(t=>t.Board)
+            .WithMany(b=>b.Tasks)
+            .HasForeignKey(t=>t.BoardId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+			base.OnModelCreating(builder);
+		}
+	}
+}
